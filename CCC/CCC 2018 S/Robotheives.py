cf5cccb8432d9bc6ef555fa0e5da3f2bb1131d71
Edwin Zheng <jessicawu414@gmail.com>
2020-02-08 20:40:01 -0500

    i know im stupid please don't judge

--- CCC/CCC 2018 S/Robotheives.py
+++ CCC/CCC 2018 S/Robotheives.py
@@ -1,14 +1,19 @@
+#------------------------------------------Input Handling------------------------------------------
+#Global Variables
 r, c = input().split(" ")
 r, c = int(r), int(c)
 sr, sc = 0, 0
-
-# Make a maze of all the values
+#Up, Right, Down, Left; clockwise motion
+directions = [[-1,0], [0,1], [1,0] ,[0,-1]]
 maze = []
+stepMaze = []
+adjList = []
+
+#----------------Make a maze of all the values----------------
 for i in range(r):
     maze.append(list(input()))
 
-##Make a separate maze for all the steps
-stepMaze = []
+#---------------------Make a separate maze for all the steps(this is output-ish)----------------
 for i in range(r):
     row = []
     for j in range(c):
@@ -16,40 +21,33 @@ for i in range(r):
             row.append(-1)
         elif maze[i][j] == "S":
             row.append(0)
-            sr = i
-            sc = j
+            sr, sc = i,j
         else:
             row.append("/")
     stepMaze.append(row)
 
-# Making an adjacency list
-adjList = []
-#Up, Right, Down, Left; clockwise motion
-directions = [[-1,0], [0,1], [1,0] ,[0,-1]]
-for row  in range(r):
-    for col in range(c):
-        toAdd = []
-        for down, right in directions:
-            newRow = row + down
-            newCol = col + right
-            if newRow < 0 or newCol < 0:
-                continue
-            if newRow  + 1 > r or newCol + 1> c:
-                continue
 
-            typeList = "counting"
-            if typeList == "counting":
-                #Counting Grid
-                toAdd.append(c * newRow + newCol)
-            elif typeList == "absolute":
-                #Absolute Position
-                toAdd.append([newRow, newCol])
-        adjList.append(sorted(toAdd))
+#------------------------------------------Processing------------------------------------------
 
-for i in adjList:
-    print(adjList.index(i), i)
+def fill(curPos, distance):
+    toExplore = []
+    for down, right in directions:
+        newRow = curPos[0] + down
+        newCol = curPos[1] + right
+        if newRow < 0 or newCol < 0:
+            continue
+        if newRow + 1 > r or newCol + 1 > c:
+            continue
+        if stepMaze[newRow][newCol] == -1:
+            stepMaze[newRow][newCol] = distance
+            toExplore.append([newRow, newCol])
+    for location in toExplore:
+        fill(location, distance + 1)
 
-q = []
+fill([sr, sc], 1)
 
-banList = ["W", "L", "R", "U", "D"]
-print(" ".join([item for row in stepMaze for item in row if item not in banList]))
+#------------------------------------------Output Handling------------------------------------------
+for row in stepMaze:
+    for item in row:
+        if item != "/" and item != 0: #not == Wall & not == Start(0)
+            print(item)
