57242ec298612c765d884fd587b4b4125bc74d62
Edwin <jessicawu414@gmail.com>
2020-11-22 23:00:21 -0500

    gitignore changes

--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-while True:
-    list = []
-    if random.uniform(0, 1) + random.uniform(0, 1) <= 1:
-        list.append(1)
-    else:
-        list.append(0)
-    print((sum(list)/len(list)))
