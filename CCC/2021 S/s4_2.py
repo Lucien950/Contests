7aaa5a60f18e8d9936e02235c5e2dd8cadca9cf2
Edwin <jessicawu414@gmail.com>
2022-01-25 12:39:41 -0500

    More work on S4

--- CCC/2021 S/s4_2.py
+++ CCC/2021 S/s4_2.py
@@ -1,14 +1,29 @@
+from email.policy import default
 from sys import stdin
+from collections import defaultdict
+
 input = stdin.readline
 n, w, d = map(int, input().split())
 walkways = [tuple(map(int, input().split())) for _ in range(w)]
 stations = {j:i for i, j in enumerate(map(int,input().split()))}
-swaps = [(0, 0), tuple(map(int, input().split())) for _ in range(d)]
+swaps = [(1, 1)]+[tuple(map(int, input().split())) for _ in range(d)]
 
+walkAdj = defaultdict(list)
+for to, f in walkways:
+    walkAdj[to].append(f)
+walkWeights = [-2]+[-1 for _ in range(n)]
+walkWeights[-1]=1
+queue = {4}
+visited = [False for _ in range(n)]
+while queue:
+    node = queue.pop()
+    for next in walkAdj[node]:
+        if visited[next]: continue
+        walkWeights[next] = walkWeights[node] + 1
+        visited[next] = True
+        queue.add(next)
 for s1, s2 in swaps:
     temp = stations[s1]
     stations[s1] = stations[s2]
     stations[s2] = temp
-
-    for station in range(1,n):
-        subwayTime = max(stations[station]-stations[n])
+    
