321eaa841f6d2af916f2ba54f841c56ff7ff3df4
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-01-26 14:44:33 -0500

    Oscar Changes

--- CCC/2021 S/s4_2.py
+++ CCC/2021 S/s4_2.py
@@ -1,5 +1,5 @@
 from sys import stdin
-from collections import defaultdict
+from collections import defaultdict, deque
 
 input = stdin.readline
 n, w, d = map(int, input().split())
@@ -12,23 +12,24 @@ for to, f in walkways:
 	walkAdj[f].append(to)
 
 # Amount of time required to navigate to the end from a given node using only walkways
-walkWeights = [float('inf')]+[float('inf') for _ in range(n)]
+walkWeights = [float('inf') for _ in range(n + 1)]
 walkWeights[-1]=0
-queue = {n}
-visited = [False for _ in range(n)]
+queue = deque(n)
+queue.append(n)
+visited = [False for _ in range(n + 1)]
 while queue:
-	node = queue.pop()
+	node = queue.popleft()
 	for next in walkAdj[node]:
 		if visited[next]: continue
 		walkWeights[next] = walkWeights[node] + 1
 		visited[next] = True
-		queue.add(next)
+		queue.append(next)
 
 # unordered list of all travel distance. Order is not relevent when looking for minimum
-travelDistance = [lastStop + walkWeights[lastStop] for lastStop in range(1, n+1)]
+travelDistance = [lastStop - 1 + walkWeights[stations[lastStop]] for lastStop in range(1, n+1)]
 for s1, s2 in swaps:
-	station1OldDist = s1 + walkWeights[stations[s1]]
-	station2OldDist = s2 + walkWeights[stations[s2]]
+	station1OldDist = s1 - 1 + walkWeights[stations[s1]]
+	station2OldDist = s2 - 1 + walkWeights[stations[s2]]
 	travelDistance.remove(station1OldDist)
 	travelDistance.remove(station2OldDist)
 
@@ -36,8 +37,8 @@ for s1, s2 in swaps:
 	stations[s1] = stations[s2]
 	stations[s2] = temp
 
-	travelDistance.append(s1 + walkWeights[stations[s1]])
-	travelDistance.append(s2 + walkWeights[stations[s2]])
+	travelDistance.append(s1 - 1 + walkWeights[stations[s1]])
+	travelDistance.append(s2 - 1 + walkWeights[stations[s2]])
 
-	subOnly = stations.index(4)-1
+	subOnly = stations.index(n)-1
 	print(min(min(travelDistance), subOnly))
