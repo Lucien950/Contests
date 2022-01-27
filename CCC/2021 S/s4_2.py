aa2722a157d2b37eb09eba5118f3a35e54e50c86
Edwin <jessicawu414@gmail.com>
2022-01-27 10:22:24 -0500

    heapq implimentation

--- CCC/2021 S/s4_2.py
+++ CCC/2021 S/s4_2.py
@@ -1,5 +1,6 @@
 from sys import stdin
 from collections import defaultdict, deque
+import heapq
 
 input = stdin.readline
 n, w, d = map(int, input().split())
@@ -26,19 +27,38 @@ while queue:
 		queue.append(next)
 
 # unordered list of all travel distance. Order is not relevent when looking for minimum
-travelDistance = [lastStop - 1 + walkWeights[stations[lastStop]] for lastStop in range(1, n+1)]
+travelDistance = []
+for lastStop in range(1, n+1):
+	heapq.heappush(travelDistance, (
+		lastStop - 1 + walkWeights[stations[lastStop]],
+		lastStop,
+		stations[lastStop]
+	))
+
 for s1, s2 in swaps:
 	station1OldDist = s1 - 1 + walkWeights[stations[s1]]
 	station2OldDist = s2 - 1 + walkWeights[stations[s2]]
-	travelDistance.remove(station1OldDist)
-	travelDistance.remove(station2OldDist)
 
-	temp = stations[s1]
-	stations[s1] = stations[s2]
-	stations[s2] = temp
+	stations[s1], stations[s2] = stations[s2], stations[s1]
 
-	travelDistance.append(s1 - 1 + walkWeights[stations[s1]])
-	travelDistance.append(s2 - 1 + walkWeights[stations[s2]])
+	heapq.heappush(travelDistance, (
+		s1 - 1 + walkWeights[stations[s1]],
+		s1,
+		stations[s1],
+	))
+	heapq.heappush(travelDistance, (
+		s2 - 1 + walkWeights[stations[s2]],
+		s2,
+		stations[s2],
+	))
 
-	subOnly = stations.index(n)-1
-	print(min(min(travelDistance), subOnly))
+	while True:
+		# ans[0] returns the smallest time (thanks heapq)
+		time, index, station = travelDistance[0]
+		# Is this still a valid station (did no swaps affect it)
+		if stations[index] == station:
+			subOnly = stations.index(n)-1
+			print(min(subOnly, time))
+			break
+		# If it is invalid, remove it
+		heapq.heappop(travelDistance)
