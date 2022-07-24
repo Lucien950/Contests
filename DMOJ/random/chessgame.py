65308770edf72c5b696f8611f956eab091be8f4d
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-07-24 00:39:13 -0400

    Chess game MLE

--- DMOJ/random/chessgame.py
+++ DMOJ/random/chessgame.py
@@ -6,70 +6,60 @@ inputList = """5 5
 .*.*.""".split("\n")
 input = lambda: inputList.pop(0)
 
-from itertools import islice
+
+def replace(list, val, newval):
+	for i in range(len(list)):
+			if list[i] == val:
+					list[i] = newval
+	return list
 height, width = map(int, input().split())
-board = [list(input()) for _ in range(height)]
-used = [[False for _ in range(width)] for _ in range(height)]
+board = [replace(list(input()), ".", False) for _ in range(height)]
 out = 0
 
-def check(iterator, flipCoords):
-	global out, used
+for flipCoords in (False, True):
+	if flipCoords:  iterator = enumerate(zip(*board))
+	else: iterator = enumerate(board)
 	# length is row/col
 	for i, length in iterator:
 		# get the index of the second instance of "*"
 		try:
 			firstPiece = length.index("*")
 			secondPiece = length.index("*", firstPiece + 1)
-		except:
-			continue
-		try:
-			thirdPiece = length.index("*", secondPiece + 1)
-		except:
-			thirdPiece = None
+		except: continue
+
+		try: thirdPiece = length.index("*", secondPiece + 1)
+		except: thirdPiece = None
 
 		if thirdPiece:
-			for j, item in enumerate(length):
-				if item == ".":
+			for j in range(len(length)):
+				item = length[j]
+				if item == False:
 					if flipCoords:
-						add = not used[j][i]
-						used[j][i] = True
+						board[j][i] = True
 					else:
-						add = not used[i][j]
-						used[i][j] = True
-					if add: out += 1
+						board[i][j] = True
+					out += 1
 		else:
-			# check backwards and forwards
-			def sliceEnum(Ilist, s, e):	return islice(enumerate(Ilist), s, e)
-
-			for itt in (sliceEnum(length, 0, firstPiece), sliceEnum(length, secondPiece + 1, width)):
-				for j, e in itt:
-					if e == ".":
+			end = height if flipCoords else width
+			for itt in (range(0, firstPiece), range(secondPiece + 1, end)):
+				for j in itt:
+					e = length[j]
+					if e == False:
 						if flipCoords:
-							add = not used[j][i]
-							used[j][i] = True
+							board[j][i] = True
 						else:
-							add = not used[i][j]
-							used[i][j] = True
-						if add: out += 1
-
-iterators = [
-	(enumerate(board), False),
-	(enumerate(zip(*board)), True),
-]
-# function
-for iterator, flipCoords in iterators:
-	check(iterator, flipCoords)
-
+							board[i][j] = True
+						out += 1
 print(out)
 
 # output
-for i, row in enumerate(used):
-	outstring = ""
-	for j, e in enumerate(row):
-		if board[i][j] == "*":
-			outstring += "⬛"
-		elif e:
-			outstring += "✅"
-		else:
-			outstring += "❌"
-	print(outstring)
+# for i, row in enumerate(used):
+# 	outstring = ""
+# 	for j, e in enumerate(row):
+# 		if board[i][j] == "*":
+# 			outstring += "⬛"
+# 		elif e:
+# 			outstring += "✅"
+# 		else:
+# 			outstring += "❌"
+# 	print(outstring)
