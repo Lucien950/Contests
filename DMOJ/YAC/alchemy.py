77a99916aeaf9468a63aada0736520a66f8546bf
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-08-18 01:02:45 -0400

    Alchemy Solution, no defaultdict �

--- DMOJ/YAC/alchemy.py
+++ DMOJ/YAC/alchemy.py
@@ -1,59 +1,98 @@
-from collections import Counter
-import sys
-input = sys.stdin.readline
+from collections import defaultdict
+
+# def solveOld(elements, elementCount):
+# 	# O
+# 	mapping = [-1 for _ in range(elementCount)] # all elements must be > 0
+
+# 	solveOrder = sorted(list(range(elementCount)), key=lambda x: elements[x])
+# 	elementCounters = Counter(elements)
+
+# 	# first instance of every element number, as to point element number + 1
+# 	firstInstance = {}
+# 	for i, k in enumerate(elements):
+# 		if k not in firstInstance:
+# 			firstInstance[k] = i
+
+# 	for solveIndex in solveOrder:
+# 		# solved
+# 		if mapping[solveIndex] != -1: continue
+# 		loopSize = elements[solveIndex]
+# 		# self looping
+# 		if loopSize == 1:
+# 			mapping[solveIndex] = solveIndex + 1 # oof
+# 			continue
+
+# 		needChildren = loopSize - 1
+# 		totalLoopAmount = elementCounters[loopSize] 
+# 		# check free point
+# 		if needChildren in firstInstance:
+# 			mapping[solveIndex] = firstInstance[needChildren] + 1 # oof
+# 		# make loops
+# 		elif totalLoopAmount % loopSize == 0:
+# 			for loopStartIndex in range(solveIndex, solveIndex + totalLoopAmount, loopSize):
+# 				for i in range(loopStartIndex, loopStartIndex + loopSize - 1):
+# 					j = solveOrder[i]
+# 					mapping[j] = solveOrder[i + 1] + 1 # oof
+# 				# join the last one to the first one
+# 				mapping[solveOrder[i + 1]] = solveOrder[loopStartIndex] + 1 # oof
+# 		else:
+# 			return -1
+
+# 	if any([i == -1 for i in mapping]): return -1
+# 	else: return " ".join(map(str, mapping))
 
 def solve(elements, elementCount):
-	# O
 	mapping = [-1 for _ in range(elementCount)] # all elements must be > 0
 
-	solveOrder = sorted(list(range(elementCount)), key=lambda x: elements[x])
-	elementCounters = Counter(elements)
-	firstInstance = {}
-	for i, k in enumerate(elements):
-		if k not in firstInstance:
-			firstInstance[k] = i
-		if len(firstInstance) == 10:
-			break
-
-	for solveIndex in solveOrder:
-		# solved
-		if mapping[solveIndex] != -1: continue
-		needChildren = elements[solveIndex] - 1
-		# self looping
-		if needChildren == 0:
-			mapping[solveIndex] = solveIndex + 1 # oof
+	# elementIndexDict = defaultdict(list)
+	elementIndexDict = [[] for _ in range(10**6+1)]
+	for i, element in enumerate(elements): elementIndexDict[element].append(i)
+	
+	# for loopSize, elementIndexes in elementIndexDict.items():
+	for loopSize, elementIndexes in enumerate(elementIndexDict):
+		if len(elementIndexes) == 0:
+			continue
+		if loopSize == 1:
+			for solveIndex in elementIndexes:
+				mapping[solveIndex] = solveIndex + 1 # oof
 			continue
 		
+		needChildren = loopSize - 1
+		totalLoopAmount = len(elementIndexes)
 		# check free point
-		if needChildren in firstInstance:
-			mapping[solveIndex] = firstInstance[needChildren] + 1 # oof
+		# if needChildren in elementIndexDict:
+		if len(elementIndexDict[needChildren]) >= 1:
+			firstLowerInstance = elementIndexDict[needChildren][0]
+			for solveIndex in elementIndexes:
+				mapping[solveIndex] = firstLowerInstance + 1 # oof
 		# make loops
-		elif elementCounters[needChildren + 1] % (needChildren + 1) == 0:
-			pairIndexes = solveOrder[solveIndex: solveIndex + elementCounters[needChildren + 1]]
-			for startIndex in range(0, len(pairIndexes), needChildren + 1):
-				sub = pairIndexes[startIndex: startIndex + needChildren + 1]
-				for i, j in enumerate(sub[:-1]):
-					mapping[j] = sub[i + 1] + 1 # oof
+		elif totalLoopAmount % loopSize == 0:
+			for loopStartIndex in range(0, len(elementIndexes), loopSize):
+				for i in range(loopStartIndex, loopStartIndex + loopSize - 1):
+					j = elementIndexes[i]
+					mapping[j] = elementIndexes[i + 1] + 1 # oof
 				# join the last one to the first one
-				mapping[sub[-1]] = sub[0] + 1 # oof
+				mapping[elementIndexes[i + 1]] = elementIndexes[loopStartIndex] + 1 # oof	
 		else:
 			return -1
 
 	if any([i == -1 for i in mapping]): return -1
 	else: return " ".join(map(str, mapping))
+
 # I
-# elementCount = int(input())
-# elements = list(map(int, input().split()))
-# print(solve(elements, elementCount))
+elementCount = int(input())
+elements = tuple(map(int, input().split()))
+print(solve(elements, elementCount))
 
 # from itertools import product
 # for p in product(range(1, 5), repeat=4):
-#     print(p)
-#     sol = solve(list(p), 4)
-#     if sol == -1: print(f"in {p} out {sol}")
+# 	sol, sol2 = solve(list(p), 4), solveNew(list(p), 4)
+# 	if sol != sol2:
+# 		print(p)
+# 		print(sol, sol2)
 
-from random import randint
-e = []
-for i in range(100000):
-	e.append(randint(1, 9))
-solve(e, 100000)
+# from random import randint
+# e = []
+# for i in range(100000):
+# 	e.append(randint(1, 9))
+# solve(e, 100000)
