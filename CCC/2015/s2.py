b7ee3129fbb355caaffe077e94ddcd9efdd43ed5
Edwin <20777515+Lucien950@users.noreply.github.com>
2022-09-21 17:33:31 -0700

    CCC Solves Spreadsheet

--- CCC/2015/s2.py
+++ CCC/2015/s2.py
@@ -1,19 +1,20 @@
-from collections import defaultdict
-sizeEnum = {'S': 0, 'M': 1, 'L': 2, '':4}
+sizeEnum = {'S': 0, 'M': 1, 'L': 2}
+
 jerseyCount = int(input())
-athleteCount = int(input())
-jerseyList = [input() for _ in range(jerseyCount)]
-athleteSizes = defaultdict(str)
-for _ in range(athleteCount):
-	size, number = input().split()
-	number = int(number)
-	if sizeEnum[size] < sizeEnum[athleteSizes[number]]:
-		athleteSizes[number] = size
+requestCount = int(input())
+
+jerseySizes = [sizeEnum[input()] for _ in range(jerseyCount)]
+requests = [input().split() for _ in range(requestCount)]
+
+smallestJerseyForNum = [float('inf') for _ in range(jerseyCount)]
+for size, number in requests:
+	size = sizeEnum[size]
+	number = int(number) - 1
+	smallestJerseyForNum[number] = min(size, smallestJerseyForNum[number])
+
 
 fulfilled = 0
-for athleteNumber in athleteSizes:
-	lookingForSize = athleteSizes[athleteNumber]
-	largestJersey = jerseyList[sizeEnum[lookingForSize]-1]
-	if sizeEnum[largestJersey] >= sizeEnum[lookingForSize]:
+for jerseyNum in range(jerseyCount):
+	if smallestJerseyForNum[jerseyNum] <= jerseySizes[jerseyNum]:
 		fulfilled += 1
 print(fulfilled)
