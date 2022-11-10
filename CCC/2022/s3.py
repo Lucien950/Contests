bf7711db4956cb440e2dfc64f076f9da5c1fb314
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-11-10 00:58:38 -0800

    CCC S3 2022 Solve

--- CCC/2022/s3.py
+++ CCC/2022/s3.py
@@ -1,29 +1,53 @@
-# For the first subtask, it suffices to attempt all possible output arrays, and run a brute force check to determine if it works. 
-# For the second subtask, note that any array of length 3 is not good. Also, all arrays of length 1 are good. Thus, the question is simply how many arrays of length 2 are good. We calculate K - N as our required count. Note that we can build it as we go. From the left, if we still need good samples, we can swap to the other pitch, otherwise, keep the same one. Note that all values between N and 2N - 1 inclusive are possible and no others are.
-# For the third subtask, we can build on our subtask 2 solution. Let K - N be our required count. Assume we have some current array A. Depending on our remaining required count, we can decide what to append to our array A. For instance, if we no longer need any good samples, we can simply add a value equal to the current last value of the array. If we need 3 more, we can add the note 4 notes back. That way, there are 3 good samples. We can also pick a new number if we want even more good samples. It suffices to greedily choose the number that gives us the most samples up to the remaining amount, because everytime we run this we have a choice of some range [0, k] of integers, so it's not as though we will reach a point where we're missing a number that we need.	
-# For an implementation detail, it's a good idea to keep track of the longest suffix of your array A that is valid.
-# The last subtask is basically the same, all we do is note that if we would add a new number to the end, we have to instead do a proper check to see if the new number is too large. After that it proceeds as before.
+def solve(totalNoteCount, maxPitch, totalSamples):
+	# if there are more notes than samples, even if you go 1 1 1...
+	# there will be too many samples
+	if totalSamples < totalNoteCount: return "-1"
 
-noteCount, maxPitch, sampleCount = map(int, input().split())
-result = "1"
-if sampleCount < noteCount:
-    print("too little samples")
-    exit()
-sampleCount -= noteCount
-i = 2
-while sampleCount >= i - 2:
-    result += str(i)
-    sampleCount -= i
 
-    i += 1
-    i %= maxPitch
-# tie it up with the last note
-sampleCount -= i - 2
-result += result[1-i]
+	notesLeft, samplesLeft = totalNoteCount, totalSamples
+	piece = [] 
+	addBy, i = 1, 1
+	# leave when cannot add full batch or need to save notes to reach end
+	while addBy <= samplesLeft:
+		notesLeft <= samplesLeft
 
-# fill rest
-result += "1" * (noteCount - len(result))
-if len(result) != noteCount:
-    print(f"result length mismatch {result} {len(result)} != {noteCount}")
-    exit()
-print(result)
+		# construction
+		piece.append(str(i))
+		samplesLeft -= addBy
+
+		# need too many notes check
+		notesLeft -= 1
+		if notesLeft < 0: return "-1"
+
+		# if need to start saving notes
+		if notesLeft > samplesLeft:
+			piece.pop(-1)
+			samplesLeft += addBy
+			notesLeft += 1
+			break
+		# prepare for next iteration
+		i = i % maxPitch + 1
+		addBy = min(addBy + 1, maxPitch)
+	# add the remainder
+	if samplesLeft > 0:
+		if notesLeft == 0: return "-1"
+		# wizard math. do not question. lots of off by 1 errors, relatively easy to calculate
+		targetSamplesLeft = notesLeft - 1
+		piece.append(piece[-(samplesLeft - targetSamplesLeft)])
+		# fill to end
+		piece += [str(piece[-1])] * max(0, (totalNoteCount - len(piece)))
+	return " ".join(piece)
+totalNoteCount, maxPitch, totalSamples = map(int, input().split())
+print(solve(totalNoteCount, maxPitch, totalSamples))
+
+# ====TESTING====
+# from random import randint
+# totalSamples, totalNoteCount = 0,1
+# while totalSamples < totalNoteCount:
+# 	totalNoteCount, maxPitch, totalSamples = (randint(1, 20), 4, randint(1, 20))
+# print(f"notes: {totalNoteCount}, max: {maxPitch}, samples: {totalSamples}")
+# assert(solve(3, 2, 5) == "1 2 1")
+# assert(solve(5, 5, 14) == "1 2 3 4 1")
+# assert(solve(5, 5, 50) == "-1")
+# assert(solve(6, 9, 10) == "1 2 3 2 2 2")
+# assert(solve(5, 3, 11) == "1 2 3 1 3")
