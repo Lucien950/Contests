6b865fbc19f46ab712ccfe262aef6c1e3ce33d5d
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-11-10 01:00:26 -0800

    10/27 solve for Jacob

---
+++ leetcode/nQueens.py
@@ -0,0 +1,90 @@
+class Solution:
+  def isSafe(self, x, y):
+    for temp_x in range(self.n):
+      if self.board[y][temp_x] == "Q": return False
+    for temp_y in range(self.n):
+      if self.board[temp_y][x] == "Q": return False
+
+    # -x, +y -> +x, -y
+    for diagVal in range(-min(x, self.n - 1 - y), min(self.n - 1 - x, y) + 1):
+      if self.board[y - diagVal][x + diagVal] == "Q": return False
+
+    # top left to bottom right
+    for diagVal in range(-min(x, y), min(self.n - x, self.n - y)):
+      if self.board[y + diagVal][x + diagVal] == "Q": return False
+    return True
+  
+  def solve(self, y):
+    if y == self.n:
+      self.ans.append(["".join(row) for row in self.board])
+      return
+    for x in range(self.n):
+      if self.isSafe(x, y):
+        self.board[y][x] = "Q"
+        self.solve(y + 1)
+        self.board[y][x] = "."
+
+  def solveNQueens(self, n: int) -> list[list[str]]:
+    self.board = [["."] * n for _ in range(n)]
+    self.n = n
+    self.ans = []
+    self.solve(0)
+    return self.ans
+
+
+from copy import deepcopy
+class Solution1:
+  n = None
+  solutions = []
+  explored = set()
+  def getNumericalPosition(self, x, y): return self.n * y + x
+  def sortTuple(self, t): return tuple(sorted(list(t)))
+
+  def placeNext(self, board, validSpaces, placesLeft, exploredRepr = ()) -> int:
+    if exploredRepr in self.explored: return
+    if placesLeft == 0:
+      sol = ["".join(row) for row in board]
+      if sol not in self.solutions: self.solutions.append(sol)
+      return
+    
+    for y in range(self.n):
+      for x in range(self.n):
+        if validSpaces[y][x] != True: continue
+        
+        self.explored.add(exploredRepr)
+
+        newBoard = deepcopy(board)
+        newBoard[y][x] = "Q"
+        
+        # clear valid spots
+        newValidSpaces = deepcopy(validSpaces)
+        for temp_x in range(self.n): newValidSpaces[y][temp_x] = False
+        for temp_y in range(self.n): newValidSpaces[temp_y][x] = False
+
+        # -x, +y -> +x, -y
+        for diagVal in range(-min(x, self.n - 1 - y), min(self.n - 1 - x, y) + 1):
+          newValidSpaces[y - diagVal][x + diagVal] = False
+
+        # top left to bottom right
+        for diagVal in range(-min(x, y), min(self.n - x, self.n - y)):
+          newValidSpaces[y + diagVal][x + diagVal] = False
+        
+        self.placeNext(newBoard, newValidSpaces, placesLeft-1, self.sortTuple(exploredRepr + (self.getNumericalPosition(x, y), )))
+  
+  def solveNQueens(self, n: int) -> list[list[str]]:
+    emptyBoard = [["." for _  in range(n)] for _ in range(n)]
+    validBoard = [[True for _ in range(n)] for _ in range(n)]
+    self.n = n
+    self.solutions = []
+    self.explored = set()
+    
+    self.placeNext(emptyBoard, validBoard, n)
+    return self.solutions
+
+s = Solution()
+sols = s.solveNQueens(4)
+
+print(sols)
+for sol in sorted(sols):
+  print("\n".join(sol))
+  print("===============")
