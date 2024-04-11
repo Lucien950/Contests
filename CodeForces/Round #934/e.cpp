8155d9b7300fb3a920b02ef1c43a76f1340189a0
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-04-10 18:53:17 -0700

    tree compass

--- CodeForces/Round #934/e.cpp
+++ CodeForces/Round #934/e.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <stdint.h>
 #include <vector>
 
 using namespace std;
@@ -12,8 +13,10 @@ struct furthestNodePathDistance {
 furthestNodePathDistance getFurthestNode(const int at, const vector<vector<int>>& adj, vector<bool>& explored) {
   int furthestNode = at, furthestDistance = 0;
   vector<int> longestPath;
-  explored.assign(at, true);
+  explored[at] = true;
   for (const auto node : adj[at]) {
+	if (explored[node])
+	  continue;
 	if (const auto [n, path, dist] = getFurthestNode(node, adj, explored); dist > furthestDistance) {
 	  furthestNode = n;
 	  furthestDistance = dist;
@@ -29,7 +32,7 @@ struct furthestNodePath {
   vector<int> path;
 };
 furthestNodePath getFurthestNode(const int start, const vector<vector<int>>& adj) {
-  auto explored = vector(adj.size(), false);
+  auto explored = vector(adj.size() + 1, false);
   auto [node, path, distance] = getFurthestNode(start, adj, explored);
   std::reverse(path.begin(), path.end());
   return {node, path};
@@ -52,6 +55,7 @@ int main() {
 
 	if (vertexCount == 1) {
 	  cout << 1 << endl;
+	  cout << 1 << " " << 0 << endl;
 	  continue;
 	}
 
@@ -60,10 +64,23 @@ int main() {
 
 	// get midpoint of diameterPath
 	const size_t diameterPathSize = diameterPath.size();
-	const int center = diameterPath.at(diameterPathSize / 2), afterCenter = diameterPath.at(diameterPathSize / 2 + 1);
-	for (size_t d = 1; d <= diameterPathSize / 2; d++) {
-	  cout << center << " " << d;
+	const int center = diameterPath.at(diameterPathSize / 2), beforeCenter = diameterPath.at(diameterPathSize / 2 - 1);
+
+	vector<pair<int, int>> out;
+	if (diameterPathSize % 2 == 0) {
+	  for (int64_t d = (int64_t)diameterPathSize / 2 - 1; d >= 0; d -= 2) {
+		out.emplace_back(center, d);
+		out.emplace_back(beforeCenter, d);
+	  }
+	} else { // odd case
+	  for (size_t d = 0; d <= diameterPathSize / 2; d++) {
+		out.emplace_back(center, d);
+	  }
 	}
-	cout << afterCenter << " " << 1 << endl;
+
+	cout << out.size() << endl;
+	for (pair<int, int> p : out) {
+	  cout << p.first << " " << p.second << endl;
+	}
   }
 }
