159f4106f8b14e16d8e8ecb1afab4862d26d5701
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-12-09 17:58:05 -0800

    fixed 3577

--- leetcode/3577.cpp
+++ leetcode/3577.cpp
@@ -14,17 +14,11 @@ class Solution {
 
 public:
   static int countPermutations(const vector<int>& complexity) {
-	// if complexity is not increasing, then return 0
-	if (complexity[0] >= complexity[1]) {
-	  return 0;
-	}
-	for (size_t i = 2; i < complexity.size(); i++) {
-	  if (complexity[i - 1] > complexity[i]) {
+	for (const auto c : complexity) {
+	  if (c <= complexity[0]) {
 		return 0;
 	  }
 	}
-	// we know that complexity is now increasing
-	// return (complexity.size() - 1)! mod 1e9 + 7
 	return factorialMod(static_cast<int>(complexity.size()) - 1);
   }
 };
